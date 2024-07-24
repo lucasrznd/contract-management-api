3a8b2b9a90d2ae533888aa633117a8c7492b97c2
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-24 11:32:59 -0300

    feat: add companies dtos

---
+++ src/main/java/br/com/lucasrznd/contractmanagementapi/dtos/response/CompanyResponse.java
@@ -0,0 +1,15 @@
+package br.com.lucasrznd.contractmanagementapi.dtos.response;
+
+public record CompanyResponse(Long id,
+                              String businessName,
+                              String tradeName,
+                              String registrationNumber,
+                              String stateRegistration,
+                              String phoneNumber,
+                              String email,
+                              String streetName,
+                              String avenueName,
+                              Integer number,
+                              String state,
+                              String zipCode) {
+}
