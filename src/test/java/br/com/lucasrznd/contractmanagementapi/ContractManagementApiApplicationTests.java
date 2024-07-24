caf94bc9f305cc43a1c96be12a8d44215cf8816e
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-24 09:31:13 -0300

    chore: create project structure

---
+++ src/test/java/br/com/lucasrznd/contractmanagementapi/ContractManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasrznd.contractmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
