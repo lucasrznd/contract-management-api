4fc747ec2f4a6c52c9507852c570ab5c2c54dd7c
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-24 11:51:48 -0300

    feat: add find by name method

--- src/main/java/br/com/lucasrznd/contractmanagementapi/repositories/ClientCompanyRepository.java
+++ src/main/java/br/com/lucasrznd/contractmanagementapi/repositories/ClientCompanyRepository.java
@@ -4,6 +4,11 @@ import br.com.lucasrznd.contractmanagementapi.entities.ClientCompany;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientCompanyRepository extends JpaRepository<ClientCompany, Long> {
+
+    Optional<ClientCompany> findByTradeName(final String tradeName);
+
 }
