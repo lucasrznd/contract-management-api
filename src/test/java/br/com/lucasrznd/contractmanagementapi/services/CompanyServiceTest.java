64170362f2d45e649d11cd542cc237e329cc825f
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-01 11:52:24 -0300

    test: add save company methods with invalid fields

--- src/test/java/br/com/lucasrznd/contractmanagementapi/services/CompanyServiceTest.java
+++ src/test/java/br/com/lucasrznd/contractmanagementapi/services/CompanyServiceTest.java
@@ -9,12 +9,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.Optional;
 
 import static br.com.lucasrznd.contractmanagementapi.creator.CreatorUtils.generateMock;
+import static br.com.lucasrznd.contractmanagementapi.utils.CompanyConstants.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CompanyServiceTest {
@@ -23,22 +28,122 @@ class CompanyServiceTest {
     private CompanyService service;
 
     @Mock
-    private ClientCompanyRepository  repository;
+    private ClientCompanyRepository repository;
 
     @Mock
     private CompanyMapper mapper;
 
     @Test
     public void saveCompany_WithValidData_ReturnsCompany() {
-        final var request = generateMock(CompanyRequest.class);
+        when(mapper.toEntity(COMPANY_REQUEST)).thenReturn(COMPANY_ENTITY);
+        when(repository.save(COMPANY_ENTITY)).thenReturn(COMPANY_ENTITY);
+        when(mapper.toResponse(COMPANY_ENTITY)).thenReturn(COMPANY_RESPONSE);
 
-        when(mapper.toEntity(request)).thenReturn(new ClientCompany());
-        when(repository.save(any(ClientCompany.class))).thenReturn(new ClientCompany());
+        var savedCompany = service.save(COMPANY_REQUEST);
 
-        service.save(request);
-
-        verify(mapper).toEntity(request);
+        assertThat(savedCompany).isNotNull();
+        verify(mapper).toEntity(COMPANY_REQUEST);
         verify(repository).save(any(ClientCompany.class));
+    }
+
+    @Test
+    public void saveCompany_WithInvalidBusinessName_ThrowsException() {
+        var request = COMPANY_REQUEST.withBusinessName("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidTradeName_ThrowsException() {
+        var request = COMPANY_REQUEST.withTradeName("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidRegistrationNumber_ThrowsException() {
+        var request = COMPANY_REQUEST.withRegistrationNumber("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidPhoneNumber_ThrowsException() {
+        var request = COMPANY_REQUEST.withStateRegistration("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidEmail_ThrowsException() {
+        var request = COMPANY_REQUEST.withEmail("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidStreetName_ThrowsException() {
+        var request = COMPANY_REQUEST.withStreetName("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidAvenueName_ThrowsException() {
+        var request = COMPANY_REQUEST.withAvenueName("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidAddressNumber_ThrowsException() {
+        var request = COMPANY_REQUEST.withNumber(null);
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidState_ThrowsException() {
+        var request = COMPANY_REQUEST.withState("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
+    }
+
+    @Test
+    public void saveCompany_WithInvalidZipCode_ThrowsException() {
+        var request = COMPANY_REQUEST.withZipCode("");
+        when(mapper.toEntity(request)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThatThrownBy(() -> service.save(request)).isInstanceOf(DataIntegrityViolationException.class);
+        verify(repository, times(0)).save(any());
+        verify(mapper, times(0)).toResponse(any());
     }
 
 }
