1866f0b8335d3fb70e00e18866d2f86d0c16bc25
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-02 20:21:06 -0300

    feat: add contract response dto

---
+++ src/main/java/br/com/lucasrznd/contractmanagementapi/dtos/response/ContractResponse.java
@@ -0,0 +1,22 @@
+package br.com.lucasrznd.contractmanagementapi.dtos.response;
+
+import br.com.lucasrznd.contractmanagementapi.entities.ClientCompany;
+import br.com.lucasrznd.contractmanagementapi.entities.enums.PaymentMethod;
+
+import java.time.LocalDate;
+
+public record ContractResponse(Long id,
+                               ClientCompany clientCompany,
+                               Integer advertisingOrder,
+                               Double spotDuration,
+                               int quantitySpotDay,
+                               LocalDate startDate,
+                               LocalDate endDate,
+                               Double monthlyPrice,
+                               int flashQuantity,
+                               String newspaperParticipation,
+                               PaymentMethod paymentMethod,
+                               Integer paymentDueDate,
+                               String observation
+) {
+}
