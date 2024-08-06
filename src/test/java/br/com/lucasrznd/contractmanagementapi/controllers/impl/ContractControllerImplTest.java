ff53b80ca88b604bc9fc0cec23b73f865922ccf3
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-06 09:24:56 -0300

    test(controller): add contract update tests

--- src/test/java/br/com/lucasrznd/contractmanagementapi/controllers/impl/ContractControllerImplTest.java
+++ src/test/java/br/com/lucasrznd/contractmanagementapi/controllers/impl/ContractControllerImplTest.java
@@ -1,5 +1,6 @@
 package br.com.lucasrznd.contractmanagementapi.controllers.impl;
 
+import br.com.lucasrznd.contractmanagementapi.controllers.exceptions.ResourceNotFoundException;
 import br.com.lucasrznd.contractmanagementapi.services.ContractService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -11,10 +12,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static br.com.lucasrznd.contractmanagementapi.common.ContractConstants.*;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -43,6 +47,25 @@ class ContractControllerImplTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.error").value(BAD_REQUEST.getReasonPhrase()))
                 .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()));
+    }
+
+    @Test
+    public void updateContract_WithExistingId_ReturnsOk() throws Exception {
+        when(service.update(1L, UPDATE_CONTRACT_REQUEST)).thenReturn(CONTRACT_RESPONSE);
+
+        mockMvc.perform(put("/contracts/" + 1).contentType(APPLICATION_JSON).content(toJson(UPDATE_CONTRACT_REQUEST)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.companyBusinessName").value(UPDATE_CONTRACT_REQUEST.clientCompany().businessName()));
+    }
+
+    @Test
+    public void updateContract_WithUnexistingId_ReturnsNotFound() throws Exception {
+        doThrow(ResourceNotFoundException.class).when(service).update(1L, UPDATE_CONTRACT_REQUEST);
+
+        mockMvc.perform(put("/contracts/" + 1).contentType(APPLICATION_JSON).content(toJson(UPDATE_CONTRACT_REQUEST)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error").value(NOT_FOUND.getReasonPhrase()))
+                .andExpect(jsonPath("$.status").value(NOT_FOUND.value()));
     }
 
     private String toJson(final Object object) throws Exception {
