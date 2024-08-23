534d1e07a112627e1b1c301789f32ac5a83195ee
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-23 08:06:27 -0300

    feat: add seller response dto

---
+++ src/main/java/br/com/lucasrznd/contractmanagementapi/dtos/response/SellerResponse.java
@@ -0,0 +1,7 @@
+package br.com.lucasrznd.contractmanagementapi.dtos.response;
+
+public record SellerResponse(Long id,
+                             String name,
+                             String phoneNumber,
+                             String urlImage) {
+}
